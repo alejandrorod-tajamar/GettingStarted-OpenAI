c418b09f2796a5cb86c0b3aae8e86e5a0a80675a
Alejandro <alejandro.rodriguezdiego@tajamar365.com>
2025-01-14 09:37:03 +0100

    Proyecto completo

--- quickstart.py
+++ quickstart.py
@@ -1,12 +1,12 @@
 import os
 from openai import AzureOpenAI
-
+ 
 client = AzureOpenAI(
-  azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
+  azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"),
   api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
   api_version="2024-02-01"
 )
-
+ 
 response = client.chat.completions.create(
     model="gpt-4o-mini", # model = "deployment_name".
     messages=[
@@ -16,5 +16,5 @@ response = client.chat.completions.create(
         {"role": "user", "content": "Do other Azure AI services support this too?"}
     ]
 )
-
+ 
 print(response.choices[0].message.content)
